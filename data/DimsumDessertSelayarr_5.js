661392594763f2f78f1a5f3be118ba410ed8e420
alikaputri1 <selayarbcm@gmail.com>
2024-11-30 15:10:56 +0800

    Add files via upload

---
+++ data/DimsumDessertSelayarr_5.js
@@ -0,0 +1,8 @@
+var json_DimsumDessertSelayarr_5 = {
+"type": "FeatureCollection",
+"name": "DimsumDessertSelayarr_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nama_Tmpt": "Dimsum&Dessert", "Photo": "galery\/Dimsum & Dessert Selayar.jpg" }, "geometry": { "type": "Point", "coordinates": [ 120.457171698455554, -6.121104769707647 ] } }
+]
+}
---
+++ data/tempatSelayar_4.js
@@ -0,0 +1,8 @@
+var json_tempatSelayar_4 = {
+"type": "FeatureCollection",
+"name": "tempatSelayar_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama tempat": "Dimsum & Dessert Selayar", "X": 120.45717, "Y": -6.121072 }, "geometry": { "type": "Point", "coordinates": [ 120.45717, -6.121072 ] } }
+]
+}
